941a433399d2d5063067da169dd361033764f816
miterran <ejade86@hotmail.com>
2017-06-17 19:03:08 -0700

    Update config.js

--- src/data/config.js
+++ src/data/config.js
@@ -1,8 +1,4 @@
-// export default {
-// 	"JSON_ODD_KEY": "a81d9ed1-4536-11e7-b6b0-0afc106a1b07",
-// 	"MONGODB_URL": "mongodb://test:test@ds121192.mlab.com:21192/diamond",
-// 	"TOKEN_SECRET": "diamond"
-// }
+
 
 export default {
 	"JSON_ODD_KEY": process.env.JSON_ODD_KEY,
